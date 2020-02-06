66f9c8ea05c7454e5d2c96b863cb486fcfa1a2e2
gcolabella <53710613+gcolabella@users.noreply.github.com>
2020-02-05 16:15:36 -0800

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 $(function() {
     var playerBoard = [];
+    var playerShips = [];
     var compBoard = [];
     var compShips = [];
     var compGuessBoard = [];
@@ -10,19 +11,19 @@ $(function() {
         this.sunk = false;
         this.squares = [];
         this.board = board;
-
+        
         this.place = function() {
-            var placed = false;
-            var vertical = Math.round(Math.random()) == 0;
-            while(!placed) {
-                if(vertical) {
-                    var c = Math.floor(Math.random() * this.board.length);
-                    var r = Math.floor(Math.random() * (this.board.length-length));
-                    for (var i = 0; i < legth; i++) {
-                        this.squares.push([r+i, c]);
-                        if(this.board[r+i][c] == "S") {
-                            this.squares = [];
-                            break;
+           var placed = false;
+           var vertical = Math.round(Math.random()) == 0;
+           while(!placed) {
+               if(vertical) {
+                   var c = Math.floor(Math.random() * this.board.length);
+                   var r = Math.floor(Math.random() * (this.board.length-length));
+                   for (var i=0; i < length; i++) {
+                       this.squares.push([r + i, c]);
+                       if(this.board[r+i][c] == "S") {
+                           this.squares = [];
+                           break;
                         }
                     }
               }else {
@@ -48,34 +49,120 @@ $(function() {
         }
         this.place();
     }
+  
+  
+         this.checkSunk = function(){
+             /* Iterates through the ship's list of quares and checks the cooresponding position in the board */
+            for (var i = 0; i< this.squares.length; i++){
+                var r = this.squares[i][0];
+                var c = this.squares[i][1];
+                /* If a square's value indicates part of a ship, we  know that the ship is not sunk and returns false */
+                if(this.board[r][c] === "S"){
+                    return false;
+                }
+            }
+             /* If the loop completes, we know that the ship is sunk and can set the ship's sunk variable to true and return true. */
+            this.sunk = true;
+            return true;
+         }
+}
+  
     function drawBoard(board,player) {
         for (var i = 0; i < board.length; i++){
             for (var j = 0; j < board[i].length; j++){
                 var color = "1AD1FF";
                 if(board[i][j] == "S" && player === "player") {
                     color = "gray";
+                } /* Use different colors to indicate hits and misses */
+                else if(board[i][j] == "H"){
+                    color = "#e60000";
+                }else if(board[i][j] == "M"){
+                    color = "#ffff1a";
                 }
                 $("#" + player + " > #" + i + "_" + j).css("background-color", color);
             }
+          }
         }
 
-    function init() {
-    playerBoard = [];
-    compBoard = [];
-    compGuessBoard = [];
-    for (var i = 0; i < 10; i++) {
-        var row = [];
-        for (var j = 0; i < 10; i++) {
-            row.push("W");
-            var id = i + "_" + j;
-            $("#player").append("<div id' = "+id+"' class = 'gridsquare'></div>");
-            $("#computer").append("<div id' = "+id+"' class = 'gridsquare'></div>");
+/* Function to see checkWon */
+    function checkWon(ships){
+        for(var i = 0; i< ships.length; i++){
+            if(!ships[i].sunk){
+                return false;
             }
+        }
+        return true;
+    }
+
+/* Function compTurn for computer turn */
+function compTurn(){
+
+}
+
+    function init() {
+        playerBoard = [];
+        playerShips = [];
+        compBoard = [];
+        compGuessBoard = [];
+        compShips = [];
+        for (var i = 0; i < 10; i++) {
+            var row = [];
+            for (var j = 0; j < 10; j++) {
+                row.push("W");
+                var id = i + "_" + j;
+                $("#player").append("<div id='"+id+"' class='gridsquare'></div>");
+                $("#computer").append("<div id='"+id+"' class='gridsquare'></div>");
+                }
          playerBoard.push(row);
 
          compBoard.push(row.slice());
 
          compGuessBoard.push(row.slice());
     }
+        playerShips.push(new Ship("Carrier", 5, playerBoard));
+            playerShips.push(new Ship("Battleship", 4, playerBoard));
+            playerShips.push(new Ship("Cruiser", 3, playerBoard));
+            playerShips.push(new Ship("Submarine", 3, playerBoard));
+            playerShips.push(new Ship("Destroyer", 2, playerBoard));
+            compShips.push(new Ship("Carrier", 5, compBoard));
+            compShips.push(new Ship("Battleship", 4, compBoard));
+            compShips.push(new Ship("Cruiser", 3, compBoard));
+            compShips.push(new Ship("Submarine", 3, compBoard));
+            compShips.push(new Ship("Destroyer", 2, compBoard));
+
+            drawBoard(playerBoard, "player");
+
+            /* Click Event on class gridSquare */
+            $("#computer > .gridsquare").click(function(){
+                                
+                var id = $(this).attr("id").split("_");
+                var r = parseInt(id[0]);
+                var c = parseInt(id[1]);
+                /******#### END 1/22 ####*****/
+        if(compBoard[r][c] === "H" || compBoard[r][c] === "M"){
+                    return;
+                } playerTurn = false;
+                if(compBoard[r][c] === "S"){
+                    compBoard[r][c] = "H";
+                    $('#ctext').html("Hit!");
+                    for(var i =0; i< compShips.length; i++){
+                        if(!compShips[i].sunk && compShips[i].checkSunk()){
+                            $('#ctext').html("You sunk my "+compShips[i].name+"!");
+                            break;
+                        }
+                }
+            }else{
+                compBoard[r][c] = "M";
+                $('#ctext').html("Miss!");
+            }
+            drawBoard(compBoard, "computer");
+            if(checkWon(compShips)){
+                $("#main-text").html("You win!")
+            }
+        }
+    });
+}
+        
+        
     }
 });
